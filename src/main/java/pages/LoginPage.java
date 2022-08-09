e1333f2d0dbf71d0533d96ddb1b9c31b77a307a4
Petr Orlyansky <petr.orlyansky@coherentsolutions.com>
2022-08-09 16:12:51 +0300

    camelCase

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -14,7 +14,7 @@ public class LoginPage {
     private By loginBtn = By.xpath("//div[@class='HeadBanner-ButtonsWrapper']/a[2]");
     private By submitBtn = By.cssSelector("[type=submit]");
     private By loginField = By.name("login");
-//    private By passwordField = By.className("Textinput-Control");
+    //    private By passwordField = By.className("Textinput-Control");
 //    I didn't use it, just for display , also I didn't use By.tagNme because the test became instability
     private By passwordField = By.xpath("//input[@name='passwd']");
     private By sigInBtn = By.id("passp:sign-in");
@@ -41,7 +41,7 @@ public class LoginPage {
         loginButton2.click();
     }
 
-    public boolean isComposeButtonDisplayed(){
+    public boolean isComposeButtonDisplayed() {
         WebDriverWait wait = new WebDriverWait(driver, 10);
         wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(composeButton));
         WebElement composeBtn = driver.findElement(composeButton);
